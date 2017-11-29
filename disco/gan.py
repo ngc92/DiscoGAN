e410e4e352c8dcfb9259dc37c752d411875100d6
ngc92 <ngc92@users.noreply.github.com>
2017-11-30 00:11:15 +0100

    gradient summaries

--- disco/gan.py
+++ disco/gan.py
@@ -96,6 +96,9 @@ def _disco_gan(input_A, input_B, generator_AB, generator_BA, discriminator_A, di
     loss_AB = _generator_loss(dfB, dffB, drfB, B, rB, rate, "GAB_loss")
     loss_BA = _generator_loss(dfA, dffA, drfA, A, rA, rate, "GBA_loss")
 
+    tf.summary.image("gan_gradient/A", tf.gradients(loss_BA, fA)[0])
+    tf.summary.image("gan_gradient/B", tf.gradients(loss_AB, fB)[0])
+
     tf.summary.histogram("dfA", dfA)
     tf.summary.histogram("drA", drA)
     tf.summary.histogram("dfB", dfB)
@@ -119,8 +122,8 @@ def _disco_gan(input_A, input_B, generator_AB, generator_BA, discriminator_A, di
 
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
-        train_step = tf.cond(tf.equal(tf.train.get_or_create_global_step() % 3, 0),
-                             optimize_discriminator, optimize_generator)
+        discriminator_step = tf.equal(tf.train.get_or_create_global_step() % 3, 0)
+        train_step = tf.cond(discriminator_step, optimize_discriminator, optimize_generator)
         with tf.control_dependencies([train_step]):
             train_step = tf.train.get_global_step().assign_add(1)
 
@@ -163,6 +166,62 @@ def _generator_loss(discriminator_logit, fake_features, real_features, real, rec
         total = (feature_matching + 0.1*discrimination) * (1.0 - rate) + rate * reconstruction
         tf.summary.scalar("total", total)
     return total
+
+
+# TODO this does not work yet
+def unet_gan(input, generator, discriminator, device_mapping, curriculum, is_training):
+    # create and summarize the inputs
+    with tf.device(device_mapping.input):
+        A, B, file_A, file_B = input()
+        tf.summary.image("A", A)
+        tf.summary.image("B", B)
+
+    # create and summarize fakes
+    with tf.device(device_mapping.genB), tf.variable_scope("Generator"):
+        fB = generator(A, is_training)
+        tf.summary.image("fB", fB)
+
+    # discriminators
+    with tf.device(device_mapping.disB), tf.variable_scope("Discriminator"):
+        dfB, dffB = discriminator(fB, is_training)
+
+    with tf.device(device_mapping.disB), tf.variable_scope("Discriminator", reuse=True):
+        drB, drfB = discriminator(B, is_training)
+
+    # now all the loss terms
+    discriminator_loss = _discriminator_loss(dfB, drB, "DB_loss")
+    generator_loss = tf.losses.sigmoid_cross_entropy(tf.ones_like(dfB), dfB, scope="discriminate",
+                                                     reduction=tf.losses.Reduction.MEAN)
+
+    # reconstruction loss
+    reconstruction_loss = tf.losses.mean_squared_error(B, fB, scope="reconstruction_loss")
+
+    tf.summary.histogram("dfB", dfB)
+    tf.summary.histogram("drB", drB)
+
+    var_gen = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Generator")
+    var_dis = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Discriminator")
+
+    optimizer_G = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5, beta2=0.999)
+    optimizer_D = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=0.5, beta2=0.999)
+
+    # Their code on GitHub uses only two distinct optimizers.
+    def optimize_generator():
+        return optimizer_G.minimize(generator_loss + reconstruction_loss, var_list=var_gen,
+                                    colocate_gradients_with_ops=True)
+
+    def optimize_discriminator():
+        return optimizer_D.minimize(discriminator_loss, var_list=var_dis)
+
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_step = tf.cond(tf.equal(tf.train.get_or_create_global_step() % 3, 0),
+                             optimize_discriminator, optimize_generator)
+        with tf.control_dependencies([train_step]):
+            train_step = tf.train.get_global_step().assign_add(1)
+
+    return DiscoGan(train_step=train_step, realA=A, realB=B, fakeA=fA, fakeB=fB, file_name_A=file_A, file_name_B=file_B)
+
 
 
 DeviceMapping = namedtuple("DeviceMapping", ("input", "genA", "genB", "disA", "disB"))
